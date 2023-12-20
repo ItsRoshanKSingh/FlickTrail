9c0412c7bc2b3383f68d8ec271e6706810e31eb9
Roshan <ItsRoshanKSingh@gmail.com>
2023-12-20 08:50:25 -0600

    Added Navbar for all Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,33 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'FlickTrail',
-  description: "FlickTrail is your go-to destination to explore the latest trailers"
-}
+  metadataBase: new URL("http://localhost:3000"),
+  title: "FlickTrail",
+  description:
+    "FlickTrail is your go-to destination to explore the latest trailers",
+  openGraph: {
+    title: "Title webtsite",
+    description: "this is the desciption",
+    image: "opengraph-image.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
