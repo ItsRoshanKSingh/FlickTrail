d1d4980aba43633007a6887e30a14f1e811a12e8
Roshan <ItsRoshanKSingh@gmail.com>
2023-12-21 18:58:00 -0600

    Add Cart

---
+++ src/lib/db/cart.ts
@@ -0,0 +1,52 @@
+import { Cart, Prisma } from "@prisma/client";
+import { prisma } from "./prisma";
+import { cookies } from "next/headers";
+
+export type CartWithPoster = Prisma.CartGetPayload<{
+  include: { items: { include: { poster: true } } };
+}>;
+
+export type ShoppingCart = CartWithPoster & {
+  size: number;
+  subtotal: number;
+};
+
+export async function createCart(): Promise<ShoppingCart> {
+  const newCart = await prisma.cart.create({
+    data: {},
+  });
+
+  //   FIXME: Add Encryption and Secure Setting in Production
+  cookies().set("localCartId", newCart.id);
+
+  return {
+    ...newCart,
+    items: [],
+    size: 0,
+    subtotal: 0,
+  };
+}
+
+export async function getCart(): Promise<ShoppingCart | null> {
+  const localCartId = cookies().get("localCartId")?.value;
+
+  const cart = localCartId
+    ? await prisma.cart.findUnique({
+        where: { id: localCartId },
+        include: { items: { include: { poster: true } } },
+      })
+    : null;
+
+  if (!cart) {
+    return null;
+  }
+
+  return {
+    ...cart,
+    size: cart.items.reduce((acc, item) => acc + item.quantity, 0),
+    subtotal: cart.items.reduce(
+      (acc, item) => acc + item.quantity * item.poster.price,
+      0
+    ),
+  };
+}
