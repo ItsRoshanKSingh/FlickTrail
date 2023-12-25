f51dc4b913844b8907d6b0f1f1f0084ae41c5f92
Roshan <ItsRoshanKSingh@gmail.com>
2023-12-24 20:52:22 -0600

    Callback to Add user Id & Merge before SignIN

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,33 @@
-import { env } from "@/lib/env";
+import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart";
 import { prisma } from "@/lib/db/prisma";
+import { env } from "@/lib/env";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth, { NextAuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
+import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  events: {
+    async signIn({ user }) {
+      await mergeAnonymousCartIntoUserCart(user.id);
+    },
+  },
 };
-// export default NextAuth(authOptions);
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
